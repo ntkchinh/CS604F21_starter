5fdca42f2eb26511a168234ebdfaa3b5fea68f6e
Jun Yuan-Murray <44013483+jyuan2pace@users.noreply.github.com>
2021-09-28 22:36:35 -0400

    Update ascii.c

--- assign2/ascii.c
+++ assign2/ascii.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 //each char is a numeric value
-//Please replace ??? with an integer(not a char) so that your code builds and prints out c = * 
+//Please replace ??? at line 6 with an integer(not a char) so that your code builds and prints out c = * 
 //Please only change ???
 int main() {
 	int ch = ???;//TODO: use an int, not a char

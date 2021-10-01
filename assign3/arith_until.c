df7ec03fe0c1d2d597b486430ae9fce0e30eb8be
Allison <af80647n@pace.edu>
2021-10-01 17:58:16 -0400

    edits to arith_until.c

--- assign3/arith_until.c
+++ assign3/arith_until.c
@@ -10,16 +10,19 @@ int main()
   scanf("%f", &radiusMil);
 
   //DO NOT touch the code above this line
-
+  //TODO: Follow the step-by-step "TODO" comments below. The variables have already been declared for you (see line 8) and the user has input a radius to be stored in radiusMil (see line 10)
   while (???) {
     //TODO: convert radiusMil(in mm) to radiusIn(in inch)
+    //your code here
     //TODO: then calculate the area(stored in areaIn) and circumference(stored in circIn) (in inch)
+    //your code here
     //TODO: print out the results 
     //  printf("Circle's area is %3.2f (sq in).\n", areaIn);
     //	printf("Its circumference is %3.2f (in).\n", circIn);
-    //TODO: when user enters zero, exit the loop
-    //TODO: when user enters any other number, store it in radiusMil again,
-    //TODO: loop back, continue to calculate area and circumference (in inch)
+    //TODO: when user enters zero, exit the loop. When user enters any other number, store it in radiusMil again, loop back, continue to calculate area and circumference (in inch)
+    //your code here
+    //TODO: replace ??? in line 14 so the loop runs as intended
+    
   }
  
   return 0;

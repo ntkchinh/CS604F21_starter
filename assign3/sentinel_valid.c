bf753f4827f8f9883231ec79f8f657626f99a9b1
Allison <af80647n@pace.edu>
2021-10-01 18:22:54 -0400

    edits to sentinel_valid.c

--- assign3/sentinel_valid.c
+++ assign3/sentinel_valid.c
@@ -1,5 +1,6 @@
-//You do not have a starter code in this question.
-//Copy your implementation of sentinel.c over.
-//Add an input validation to make sure each grade must be within the range of [0,100]
-//or the number will be discarded, and the code continue to ask for next grade.
-//If the number is within the valid range, then the rest is the same as sentinel.c 
+//You do not have a starter code in this question. Follow the instructions below.
+//TODO: Copy your implementation of sentinel.c over.
+//TODO: Add an input validation to make sure each grade is within the 
+//range of [0,100].  If the number is not within that range, it will be discarded, and 
+//the code will continue to ask for next grade.  If the number is within the valid range, 
+//then the rest is the same as sentinel.c 

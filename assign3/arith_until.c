884ad7676e59139e455740537c912367e8a1e48c
jun <jyuan2@pace.edu>
2021-10-01 14:43:26 -0400

    starter of assign3

---
+++ assign3/arith_until.c
@@ -0,0 +1,27 @@
+/*  Example: C program to find area of a circle */
+
+#include <stdio.h>
+
+int main()
+{
+
+  float radiusMil, radiusIn, areaIn, circIn;
+  printf("Enter radius (in mm):\n");
+  scanf("%f", &radiusMil);
+
+  //DO NOT touch the code above this line
+
+  while (???) {
+    //TODO: convert radiusMil(in mm) to radiusIn(in inch)
+    //TODO: then calculate the area(stored in areaIn) and circumference(stored in circIn) (in inch)
+    //TODO: print out the results 
+    //  printf("Circle's area is %3.2f (sq in).\n", areaIn);
+    //	printf("Its circumference is %3.2f (in).\n", circIn);
+    //TODO: when user enters zero, exit the loop
+    //TODO: when user enters any other number, store it in radiusMil again,
+    //TODO: loop back, continue to calculate area and circumference (in inch)
+  }
+ 
+  return 0;
+
+}
---
+++ assign3/termination.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int i;
+	int sum;
+	
+	//TODO: please replace ??? to calculate the sum of all the odd numbers from 1 to 1000:
+	//TODO: 1+3+5...+999
+	//TODO: you are only allowed to modify ??? please do not try to change < to <=.
+	for(i=???; i<???; ???) {
+		sum = sum + i;
+	}
+	printf("the sum is %d\n", sum);
+	return 0;
+}

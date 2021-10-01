33a10999f569f9d9ff5362fe1612f4f1b89e1285
Allison <af80647n@pace.edu>
2021-10-01 17:44:50 -0400

    more merge conflicts

--- assign2/area_circ.c
+++ assign2/area_circ.c
@@ -3,5 +3,12 @@
 #include<stdio.h>
 int main()
 {
-    float _r;
+    float r;
     printf("enter radius :\n");
+    scanf("%f", &r);
+    //r stores radius 
+    //TODO: 1. declare a variable a to hold area and a variable c to hold circumference
+    //TODO  2. calculate and store area into a and circumference into c (you can assume pi = 3.14)
+    printf("Area = %f circumference = %f\n",a,c);
+    return 0;
+}

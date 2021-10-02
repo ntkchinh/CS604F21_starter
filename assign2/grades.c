1a149a4bf781f551b31483da4c0e1138ebe43f83
Allison <af80647n@pace.edu>
2021-10-01 21:09:26 -0400

    missing lines in grades.c

--- assign2/grades.c
+++ assign2/grades.c
@@ -4,7 +4,15 @@
 int main()
 {
     //TODO: please declare the right types for variables by
-    //replacing ??? with types (in lines 8 and 9)
-    int _a, _b, _c, _d, _e;
-    float _total, _average;
+    //replacing ??? with types
+    ??? a,b,c,d,e;
+    ??? total,average;
     printf("please enter grades of 5 subjects : \n");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    //TODO calculate total and average of a student's grade
+    //TODO again beware of integer division. If you enter 1,2,2,2,2 then average is expected to be 1.8 instead of 1 but integer division will yield 1.
+    printf("Total grade = %f\n", total);
+    printf("Average grade = %f\n",average );
+    return 0;
+
+}

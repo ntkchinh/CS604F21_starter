a5b40b06eb0986bf7f1811b9ba5442c649f0e04e
Jun Yuan-Murray <44013483+jyuan2pace@users.noreply.github.com>
2021-10-08 21:41:00 -0400

    Update leaderboard.c

--- assign4/leaderboard.c
+++ assign4/leaderboard.c
@@ -33,6 +33,7 @@ int main(void) {
 	struct stud students[20];
 	int stud_count = 0;
 	char temp_name[50];
+	float grade = 0;
 	printf("Enter a test score(-1 to quit), or\n");
 	printf("Enter a grade first, then a student's name\n");
 	scanf("%f", &grade);
